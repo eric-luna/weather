7c3429e1b07de8a085f239e1304850eccdbd8bd6
Eric <ericluna1992@gmail.com>
2017-01-26 01:22:37 -0600

    removed uneccessary file

--- app/forecast/forecast.component.js
+++ app/forecast/forecast.component.js
@@ -1,9 +1,8 @@
-// Register the `phoneList` component on the `phoneList` module,
 angular.
   module('forecast').
   component('forecast', {
     templateUrl:'forecast/forecast.template.html',
-    controller: function PhoneListController($http) {
+    controller: function forecastController($http) {
 	    var self = this;
 	    self.visibility = "hidden"
 
@@ -14,30 +13,31 @@ angular.
 				    url: 'http://api.wunderground.com/api/d09806e16c9d36eb/conditions/q/' + state + '/' + city + '.json'
 				}).then(function successCallback(response) {
 				    self.error = response.data.response.error;
+				    self.current = response.data.current_observation;
 
-	                if(self.error === undefined){
+	                if(self.error === undefined && self.current !== undefined){
 	                	self.visibility = "show";
-	                	self.current = response.data.current_observation;
+	                	
+	                	console.log(self.current);
 
 	                	$http.get('http://api.wunderground.com/api/d09806e16c9d36eb/forecast10day/q/' + state + '/' + city + '.json').then(function(response) {
 	                    	self.forecast = response.data.forecast.simpleforecast.forecastday;
 	               		});
+
+	               		$http.get('./forecast/icons.json').then(function(response) {
+	                    	self.response = response.data;
+	                    	self.icon = self.current.icon_url;
+
+		                    angular.forEach(self.response, function(value, key) {
+		                        if (self.icon === key) {
+		                            self.currentIcon = value;
+		                        }
+		                    });
+	               		 });
 	                }else{
 	                	self.visibility = "hidden";
 	                	alert('There is no information for the city you are looking for.');
-	                }
-	                
-
-	                $http.get('./forecast/icons.json').then(function(response) {
-	                    self.response = response.data;
-	                    self.icon = self.current.icon_url;
-
-	                    angular.forEach(self.response, function(value, key) {
-	                        if (self.icon === key) {
-	                            self.currentIcon = value;
-	                        }
-	                    });
-	                });
+	                }    
 				}, function errorCallback(response) {
 				    alert(response.statusText);
 				});	           
